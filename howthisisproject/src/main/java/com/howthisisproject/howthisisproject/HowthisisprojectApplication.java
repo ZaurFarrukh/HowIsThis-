3cd61a8e5c75ae411bd72d8d16e299c1bef1df8d
ZaurFarrukh <zaur.farrukh.job@gmail.com>
2022-10-16 12:44:37 +0500

    How is This project

---
+++ howthisisproject/src/main/java/com/howthisisproject/howthisisproject/HowthisisprojectApplication.java
@@ -0,0 +1,13 @@
+package com.howthisisproject.howthisisproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowthisisprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowthisisprojectApplication.class, args);
+	}
+
+}
---
+++ howthisisproject/src/test/java/com/howthisisproject/howthisisproject/HowthisisprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howthisisproject.howthisisproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowthisisprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
